8b997f096538c0ab062b7692df0c582885020699
Ceasar15 <ceasarkwadwo@gmail.com>
2021-08-28 09:48:43 +0000

    thanks chale

--- mealMaker.js
+++ mealMaker.js
@@ -1,4 +1,4 @@
-// menu object 
+// menu object
 const menu = {
     _courses: {
         _appetizers: [],
@@ -32,11 +32,10 @@ const menu = {
     // getter function to return object
     get courses() {
         return {
-
             appetizers: this.appetizers,
             mains: this.mains,
             desserts: this.desserts,
-        }
+        };
     },
     // create dish method
     addDishToCourse: function (courseName, dishName, dishPrice) {
@@ -44,53 +43,51 @@ const menu = {
             name: dishName,
             price: dishPrice,
         };
-        
-        this._courses[courseName].push(dish);
 
+        this._courses[courseName].push(dish);
     },
-    // get randome dish from course 
+    // get randome dish from course
     getRandomDishFromCourse: function (courseName) {
         let dishes = this._courses[courseName];
         const randomIndex = Math.floor(Math.random() * dishes.length);
-        
-        return dishes ? dishes[randomIndex]: null;
+
+        return dishes ? dishes[randomIndex] : null;
     },
 
-    // generate random meal 
+    // generate random meal
     generateRandomMeal: function () {
-        const appetizer = this.getRandomDishFromCourse('appetizers');
-        const main = this.getRandomDishFromCourse('mains');
-        const desserts = this.getRandomDishFromCourse('desserts');
+        const appetizer = this.getRandomDishFromCourse("appetizers");
+        const main = this.getRandomDishFromCourse("mains");
+        const desserts = this.getRandomDishFromCourse("desserts");
 
         if (!appetizer) {
-          return 'No appetizer available';
+            return "No appetizer available";
         }
         if (!main) {
-          return 'No main dish is available';
+            return "No main dish is available";
         }
         if (!desserts) {
-          return 'No dessert is available';
+            return "No dessert is available";
         }
         const totalPrice = appetizer.price + main.price + desserts.price;
 
         // return string
-        return `Your main dish is ${main.name}, appetizer is ${appetizer.name}, dessert is ${desserts.name}. The total price is $${totalPrice} `; 
-    }
+        return `Your main dish is ${main.name}, appetizer is ${appetizer.name}, dessert is ${desserts.name}. The total price is $${totalPrice} `;
+    },
 };
 
+menu.addDishToCourse("appetizers", "Orange Drink", 43.9);
+menu.addDishToCourse("mains", "Jollof Rice", 47.9);
+menu.addDishToCourse("desserts", "Kelewele", 49.9);
+menu.addDishToCourse("appetizers", "Apple Drink", 41.9);
+menu.addDishToCourse("mains", "Fried Rice", 47.9);
+menu.addDishToCourse("desserts", "Abotsi", 44.9);
+menu.addDishToCourse("appetizers", "Mango Drink", 46.4);
+menu.addDishToCourse("mains", "Waakye Rice", 46.8);
+menu.addDishToCourse("desserts", "Brad", 46.9);
+menu.addDishToCourse("appetizers", "Date Drink", 46.5);
+menu.addDishToCourse("mains", "Fufu Rice", 46.9);
+menu.addDishToCourse("desserts", "Brukina", 45.9);
 
-menu.addDishToCourse('appetizers', 'Orange Drink', 43.9);
-menu.addDishToCourse('mains', 'Jollof Rice', 47.9);
-menu.addDishToCourse('desserts', 'Kelewele', 49.9);
-menu.addDishToCourse('appetizers', 'Apple Drink', 41.9);
-menu.addDishToCourse('mains', 'Fried Rice', 47.9);
-menu.addDishToCourse('desserts', 'Abotsi', 44.9);
-menu.addDishToCourse('appetizers', 'Mango Drink', 46.4);
-menu.addDishToCourse('mains', 'Waakye Rice', 46.8);
-menu.addDishToCourse('desserts', 'Brad', 46.9);
-menu.addDishToCourse('appetizers', 'Date Drink', 46.5);
-menu.addDishToCourse('mains', 'Fufu Rice', 46.9);
-menu.addDishToCourse('desserts', 'Brukina', 45.9);
-
-let meal = menu.generateRandomMeal()
+let meal = menu.generateRandomMeal();
 console.log(meal);
--- rock-paper.js
+++ rock-paper.js
@@ -1,78 +1,77 @@
 // get user input
-const getUserChoice = userInput => {
-    // convert user input to lower case
-    userInput = userInput.toLowerCase();
-    // return correct UserInput
-    if (userInput === 'bomb' || userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
-      return userInput;
-    }
-    else {
-      // log error for incorrect input
-      console.log('Enter the correct user choice: rock,scissors,paper')
-    }
-  };
-  
-  // get Computer choice
-  function getComputerChoice(){
-   let computerChoice = Math.floor(Math.random() * 3 )
-   switch (computerChoice){
-     case 0:
-     return 'rock';
-     break;
-     case 1:
-     return 'paper';
-     break;
-     case 2:
-     return 'scissors';
-     break;
-     default:
-     console.log('No number chosen');
-   }
-  };
-  
-  // function to determine the winner of the game
-  function determineWinner(userChoice, computerChoice){
-    if ( userChoice === 'bomb' ){
-      return 'User Won!'
-    }
-    if ( userChoice === computerChoice ){
-      return 'The game is a tie';
-    }
-    if (userChoice === 'rock'){
-      if (computerChoice === 'paper'){
-        return 'Computer is the winner';
-      }
-      else {
-        return 'User Won!';
-      }
-    }
-    if (userChoice === 'paper'){
-      if (computerChoice === 'scissors' || computerChoice === 'rock'){
-        return 'Computer is the winner';
-      }
-      else{
-        return 'The game is a tie';
-      }
-    }
-    if (userChoice === 'scissors'){
-      if (computerChoice === 'rock' || computerChoice === 'paper'){
-        return 'User won!';
-      }
-      else {
-        'This is a tie';
-      }
-    }
-  
-  };
-  
-  // call all functions and start game
-  function playGame(){
-    userChoice = getUserChoice('bomb');
-    console.log('User Choice: ' + userChoice);
-    computerChoice = getComputerChoice();
-    console.log('Computer Choice: ' + computerChoice);
-    console.log(determineWinner(userChoice,computerChoice));
+const getUserChoice = (userInput) => {
+  // convert user input to lower case
+  userInput = userInput.toLowerCase();
+  // return correct UserInput
+  if (
+    userInput === "bomb" ||
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    // log error for incorrect input
+    console.log("Enter the correct user choice: rock,scissors,paper");
   }
-  
-  playGame()
-  
+};
+
+// get Computer choice
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  switch (computerChoice) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+      break;
+    default:
+      console.log("No number chosen");
+  }
+}
+
+// function to determine the winner of the game
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === "bomb") {
+    return "User Won!";
+  }
+  if (userChoice === computerChoice) {
+    return "The game is a tie";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "Computer is the winner";
+    } else {
+      return "User Won!";
+    }
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors" || computerChoice === "rock") {
+      return "Computer is the winner";
+    } else {
+      return "The game is a tie";
+    }
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock" || computerChoice === "paper") {
+      return "User won!";
+    } else {
+      ("This is a tie");
+    }
+  }
+}
+
+// call all functions and start game
+function playGame() {
+  userChoice = getUserChoice("bomb");
+  console.log("User Choice: " + userChoice);
+  computerChoice = getComputerChoice();
+  console.log("Computer Choice: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
